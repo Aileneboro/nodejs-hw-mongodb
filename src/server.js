ddd7c4ffbb7a8228e18cce1af699a81142afc128
Aileneboro <a.borovikova89@gmail.com>
2024-05-31 18:49:54 +0300

    fix server

--- src/server.js
+++ src/server.js
@@ -26,6 +26,8 @@ export const setupServer = () => {
       const contacts = await getAllContacts();
 
       res.status(200).json({
+        status: 'success',
+        message: 'Successfully found contacts!',
         data: contacts,
       });
     } catch (error) {
@@ -38,9 +40,18 @@ export const setupServer = () => {
       const { contactId } = req.params;
       const contact = await getContactsById(contactId);
 
-      res.status(200).json({
-        data: contact,
-      });
+      if (!contact) {
+        res.status(404).json({
+          status: 'error',
+          message: `Contact with id ${contactId} not found`,
+        });
+      } else {
+        res.status(200).json({
+          status: 'success',
+          message: 'Successfully found contacts!',
+          data: contact,
+        });
+      }
     } catch (error) {
       next(error);
     }
