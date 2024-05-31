f7774d772198cfb74fc231166598b5b55c735863
Aileneboro <a.borovikova89@gmail.com>
2024-05-31 18:41:45 +0300

    contactsDB

---
+++ src/db/models/contacts.js
@@ -0,0 +1,42 @@
+import { Schema, model } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['personal', 'home', 'work'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
