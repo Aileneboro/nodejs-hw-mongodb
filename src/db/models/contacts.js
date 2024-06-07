0da9c95dddf3d7937e464a0b707c7530be44ce54
Aileneboro <a.borovikova89@gmail.com>
2024-06-07 13:51:07 +0300

    upd

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -24,14 +24,6 @@ const contactsSchema = new Schema(
       enum: ['work', 'home', 'personal'],
       default: 'personal',
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    updatedAt: {
-      type: Date,
-      default: Date.now,
-    },
   },
   {
     timestamps: true,
