76d0b67bb5cfa2aa3b732fcbd8290aff3a68d8e7
Aileneboro <a.borovikova89@gmail.com>
2024-06-08 14:19:23 +0300

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,6 +7,8 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
 
 const router = Router();
 
@@ -14,9 +16,17 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactsController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactsController),
+);
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:contactId',
+  validateBody(createContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactsController));
 
---
+++ src/validation/contacts.js
@@ -0,0 +1,26 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least 3 characters',
+    'string.max': 'Username should have at most 20 characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Phone number should be a string',
+    'string.min': 'Phone number should have at least 3 characters',
+    'string.max': 'Phone number should have at most 20 characters',
+    'any.required': 'Phone number is required',
+  }),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean().required(),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .required()
+    .messages({
+      'any.required': 'Contact type is required',
+    }),
+});
